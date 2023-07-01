5184197ad120acde119cd92c7999c7e7d00c0fb5
TJ Klimoski <tjklimoski@gmail.com>
2023-07-01 16:26:21 -0400

    Enable cors

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:5173'
+}))
 
 app.listen(PORT, () => console.log(`App running on port ${PORT}`))
