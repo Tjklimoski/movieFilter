acc97d305df25d77d5140f342eb2f9c297de93e7
TJ Klimoski <tjklimoski@gmail.com>
2023-07-01 16:20:00 -0400

    Setup basic express server

--- server/server.ts
+++ server/server.ts
@@ -5,4 +5,4 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.listen(PORT, (err) => { !err && console.log(`App running on port ${PORT}`) })
+app.listen(PORT, () => console.log(`App running on port ${PORT}`))
