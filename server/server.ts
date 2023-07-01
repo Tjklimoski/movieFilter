a5b943dc7795aad6520350d89c8e7c7763bc122a
TJ Klimoski <tjklimoski@gmail.com>
2023-07-01 17:12:00 -0400

    Setup axios, make get request to TMDB for popular movies and return results

--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,35 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import axios from 'axios';
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+const TMDBApi = axios.create({
+  baseURL: 'https://api.themoviedb.org/3',
+  params: {
+    api_key: process.env.API_KEY
+  }
+})
+
 app.use(cors({
-  origin: 'http://localhost:5173'
+  origin: 'http://localhost:5173',
+  methods: ['GET']
 }))
+
+app.get('/popular-movies', (req, res) => {
+
+  //get request to TMDB for their list of popular movies
+  TMDBApi.get('/movie/popular', {
+    params: {
+      api_key: process.env.API_KEY
+    }
+  })
+    .then(res => res.data.results)
+    .then(results => res.send(results))
+    .catch(err => res.status(400).send())
+})
 
 app.listen(PORT, () => console.log(`App running on port ${PORT}`))
