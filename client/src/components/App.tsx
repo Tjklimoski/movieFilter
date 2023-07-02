9a3c13f63b600ec869df62eda5d6fe0f5dee3be2
TJ Klimoski <tjklimoski@gmail.com>
2023-07-01 22:40:55 -0400

    Create filtered movies array

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,17 +1,28 @@
+import { useState, useMemo } from "react";
 import Filters from "./Filters";
 import Movies from "./Movies";
 import useTMDB from "../hooks/useTMDB";
 
 function App() {
   const [movies, loading, error] = useTMDB();
+  // genre is a number, as specifed by TMDB, 0 is all.
+  const [genre, setGenre] = useState(0);
+  const filteredMovies = useMemo(() => {
+    if (genre === 0) return movies;
+    return movies.filter(movie => movie.genre_ids.includes(genre))
+  }, [genre, movies]);
+
+  function updateGenre(genre: number) {
+    setGenre(genre)
+  }
 
   return <>
     {loading && <h1>LOADING</h1>}
     {error && <p>{error}</p>}
     {!loading &&
     <div className="page-wrapper">
-      <Filters />
-      <Movies movies={movies}/>
+      <Filters updateGenre={updateGenre}/>
+      <Movies movies={filteredMovies}/>
     </div>
     }
   </>
--- client/src/components/Filters.tsx
+++ client/src/components/Filters.tsx
@@ -1,11 +1,21 @@
 
-export default function Filters() {
+type FiltersProps = {
+  updateGenre: (genre: number) => void
+}
+
+export default function Filters({ updateGenre }: FiltersProps) {
+  // genre number is specified by TMDB's API
+  // all - 0
+  // action - 28
+  // comedy - 35
+  // horror - 27
+
   return (
     <div id="filter-btns">
-      <button className="btn" data-active="true">All</button>
-      <button className="btn">Action</button>
-      <button className="btn">Comedy</button>
-      <button className="btn">Horror</button>
+      <button className="btn" onClick={() => updateGenre(0)} data-active="true">All</button>
+      <button className="btn" onClick={() => updateGenre(28)}>Action</button>
+      <button className="btn" onClick={() => updateGenre(35)}>Comedy</button>
+      <button className="btn" onClick={() => updateGenre(27)}>Horror</button>
     </div>
   )
 }
